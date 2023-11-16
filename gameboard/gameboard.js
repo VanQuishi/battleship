9018be8f4d20e21738fb0f1401f23156a7e15295
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2023-11-16 00:06:08 -0600

    test placeShip()

--- gameboard/gameboard.js
+++ gameboard/gameboard.js
@@ -1,5 +1,13 @@
 class Gameboard {
+  constructor() {
+    this.board = [];
+  }
 
+  placeShip(ship, location) {
+    for (let i = 0; i < location.length; i++) {
+      this.board[location[i][0], location[i][1]] = ship;
+    }
+  }
 }
 
 module.exports = Gameboard
--- gameboard/gameboard.test.js
+++ gameboard/gameboard.test.js
@@ -1,6 +1,22 @@
 const Gameboard = require('./gameboard');
 
-test('gameboard obj is an instanc of Gameboard class', () => {
-  var gb = new Gameboard();
+var gb;
+
+beforeEach(() => {
+  gb = new Gameboard();
+})
+
+test('gameboard obj is an instance of Gameboard class', () => {
   expect(gb).toBeInstanceOf(Gameboard);
+})
+
+test('place ship obj at a specified location', () => {
+  var gb = new Gameboard();
+  var ship = {"hits": 0, "length": 3};
+  var location = [[0,0], [0,1], [0,2]];
+  gb.placeShip(ship, location);
+  
+  expect(gb.board[0,0]).toBe(ship);
+  expect(gb.board[0,1]).toBe(ship);
+  expect(gb.board[0,2]).toBe(ship);
 })
